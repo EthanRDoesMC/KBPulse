dbc43919c48b1794411a512675f6e008f55cc153
Ethan Chaffin <me@ethanrdoesmc.com>
2021-10-20 14:02:22 -0500

    Initial Commit

---
+++ kbbright/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  kbbright
+//
+//  Created by Ethan Chaffin on 10/20/21.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
